5c7bbef2ee1ff5a05ce2c2820efebfa3c2278262
Geoffrey Womeldorff <womeld@lanl.gov>
2019-09-03 16:54:57 -0600

    Add example usage of flcl showing an AXPY in both F and with Kokkos.

--- examples/01-axpy/axpy-main.f90
+++ examples/01-axpy/axpy-main.f90
@@ -36,18 +36,68 @@
 ! SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 program example_axpy
-    use, intrinsic :: iso_c_binding
-    use, intrinsic :: iso_fortran_env
-  
-    use :: flcl_mod
-    use :: axpy_f_mod
-  
-    implicit none
-  
-    call kokkos_initialize()
-  
-    call axpy(v_y, v_x, a)
-  
-    call kokkos_finalize()
-  
-  end program example_axpy
+  use, intrinsic :: iso_c_binding
+  use, intrinsic :: iso_fortran_env
+
+  use :: flcl_mod
+  use :: axpy_f_mod
+
+  implicit none
+
+  real(c_double), dimension(:), allocatable :: f_y
+  real(c_double), dimension(:), allocatable :: c_y
+  real(c_double), dimension(:), allocatable :: x
+  real(c_double) :: alpha
+  integer :: mm = 5000
+  integer :: ii
+
+  ! allocate memory for arrays
+  write(*,*)'allocating memory'
+  allocate(f_y(mm))
+  allocate(c_y(mm))
+  allocate(x(mm))
+
+  ! initialize kokkos
+  write(*,*)'initializing kokkos'
+  call kokkos_initialize()
+
+  ! put some random numbers in the vectors
+  ! so we aren't getting optimized out of existence
+  write(*,*)'setting up arrays'
+  call random_seed()
+  call random_number(f_y)
+  do ii = 1,mm
+    c_y(ii) = f_y(ii)
+  end do
+  call random_number(x)
+  call random_number(alpha)
+
+  ! check to see if arrays are "the same"
+  if ( norm2(f_y-c_y) < (1.0e-14)*mm ) then
+    write(*,*)'PASSED f_y and c_y the same before axpys'
+  else
+    write(*,*)'FAILED f_y and c_y the same before axpys'
+  end if
+
+  ! perform an axpy in fortran
+  write(*,*)'performing an axpy in fortran'
+  do ii = 1, mm
+    f_y(ii) = f_y(ii) + alpha * x(ii)
+  end do
+
+  ! call the f interface to the c_axpy routine
+  write(*,*)'performing an axpy with kokkos'
+  call axpy(c_y, x, alpha)
+
+  ! check to see if arrays are "the same"
+  if ( norm2(f_y-c_y) < (1.0e-14)*mm ) then
+    write(*,*)'PASSED f_y and c_y the same after axpys'
+  else
+    write(*,*)'FAILED f_y and c_y the same after axpys'
+  end if
+
+  ! finalize kokkos
+  write(*,*)'finalizing kokkos'
+  call kokkos_finalize()
+
+end program example_axpy
