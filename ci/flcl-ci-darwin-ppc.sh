062b1ef7aa04609c6e00f2e4d784e582ffd4b657
Geoffrey Womeldorff <womeld@lanl.gov>
2019-10-16 12:43:43 -0600

    Add new CI configurations.

--- ci/flcl-ci-darwin-ppc.sh
+++ ci/flcl-ci-darwin-ppc.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
+#!/bin/tcsh
 module purge
-module load intel
-export KOKKOS_ROOT=$HOME/kt/2.9-intel-serial
-cd $HOME/kokkos-fortran-interop/build
-make clean
-make libflcl.a
-make test-flcl.x
-$HOME/kokkos-fortran-interop/build/test-flcl.x
-module purge
-module load intel
-export KOKKOS_ROOT=$HOME/kt/2.9-intel-openmp
+module load gcc
+setenv KOKKOS_ROOT $HOME/kt/2.9-ppc-gnu-serial
 cd $HOME/kokkos-fortran-interop/build
 make clean
 make libflcl.a
@@ -17,8 +9,16 @@ make test-flcl.x
 $HOME/kokkos-fortran-interop/build/test-flcl.x
 module purge
 module load gcc
-module load cuda
-export KOKKOS_ROOT=$HOME/kt/2.9-gnu-cuda
+setenv KOKKOS_ROOT $HOME/kt/2.9-ppc-gnu-openmp
+cd $HOME/kokkos-fortran-interop/build
+make clean
+make libflcl.a
+make test-flcl.x
+$HOME/kokkos-fortran-interop/build/test-flcl.x
+module purge
+module load gcc
+module load cuda/10.1
+setenv KOKKOS_ROOT $HOME/kt/2.9-ppc-gnu-cuda-7.0
 cd $HOME/kokkos-fortran-interop/build
 make clean
 make libflcl.a
